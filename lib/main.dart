438dacf405c05321171b7662ea63484c2cad0084
Rajatcodehustler <97685441+Rajatcodehustler@users.noreply.github.com>
2022-01-24 00:28:44 +0530

    basic flutter app for magic ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('Ask me anything'), backgroundColor: Colors.blue[900]),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: magicball(),
+        ),
+      ),
+    ),
+  );
+}
+
+class magicball extends StatefulWidget {
+  @override
+  _magicballState createState() => _magicballState();
+}
+
+class _magicballState extends State<magicball> {
+  int mb=1;
+  void buttonpress() {
+    setState(() {
+      mb = Random().nextInt(5)+1;
+
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: TextButton( onPressed: (){
+        buttonpress();
+      },
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.asset('images/ball$mb.png'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
